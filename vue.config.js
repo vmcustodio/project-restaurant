b040678fa1b3a992fe4ba92d6b2524983448e917
vmcustodio <van.custodio.vc@gmail.com>
2022-07-31 23:54:51 -0300

    first commit

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,27 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+// vue.config.js
+module.exports = {
+  css: {
+    loaderOptions: {
+      less:{
+        additionalData: `@import "@/assets/css/variables.less";`
+      }
+    }
+  },
+  chainWebpack: config => {
+    config.module.rules.delete("svg");
+  },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.svg$/, 
+          loader: 'vue-svg-loader', 
+        },
+      ],
+    }      
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          'babel-loader',
+          'vue-svg-loader',
+        ],
+      },
+    ]
+  }
+}
